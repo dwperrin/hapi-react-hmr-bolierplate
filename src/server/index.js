77cffc5489373bf27f73eba9b136fb6bef658427
dwperrin <david@dwperrin.com>
2016-09-16 13:04:43 +0200

    add server start log

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 import Glue from 'glue'
 import { manifest } from '~/server/config/manifest'
+import { forEach } from 'ramda'
 
 Glue.compose(manifest, { relativeTo: __dirname })
-    .then(server => {
-      server.start()
-    })
+    .then(server => { return server.start().then(() => { return server }) })
+    .then(server => forEach(c => logServerConnection(c), server.connections))
     .catch(err => console.error(err))
+
+function logServerConnection (conn) {
+  console.log(`Server ${conn.settings.labels} stared at: ${conn.info.uri}`)
+}
